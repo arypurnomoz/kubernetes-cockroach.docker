4a0350152b8a47f54a9e59f4bc128040c514e583
arypurnomoz <arypurnomoz@gmail.com>
2015-08-01 11:57:50 +0700

    add selector

--- run.sh
+++ run.sh
@@ -7,9 +7,14 @@ if [ ! "$KUBE_MASTER" ]; then
   exit 1
 fi
 
+if [ ! "$SELECTOR" ]; then 
+  echo env SELECTOR is required
+  exit 1
+fi
+
 ROACH_ADDRESS=`ifconfig eth0|grep 'inet addr'|cut -d: -f2| awk '{print $1}'`
 
-PEER_NODES=`wget $KUBE_MASTER/api/v1/pods?labelSelector=app=cockroach -qO -|grep podIP|awk '{print $2}'|sed 's/"//g'|sed 's/,/:8080/'|xargs echo`
+PEER_NODES=`wget $KUBE_MASTER/api/v1/pods?labelSelector=$SELECTOR -qO -|grep podIP|awk '{print $2}'|sed 's/"//g'|sed 's/,/:8080/'|xargs echo`
 PEER_NODES=`echo $PEER_NODES|sed 's/ /,/'`
 
 echo peers $PEER_NODES
@@ -17,7 +22,6 @@ echo peers $PEER_NODES
 if [ "$ROACH_IDENDITY" ]; then 
   echo $ROACH_IDENDITY > /store/IDENTITY
 fi
-
 
 
 exec /cockroach/cockroach start --addr=${ROACH_ADDRESS}:8080 --gossip=tcp=${PEER_NODES} $@ 
